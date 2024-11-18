773e882a1abd31b4eacfedf261efb36f35cc3226
Alyona Domchevskaya <alena181823@gmail.com>
2024-11-18 14:23:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mode-contentful
+
+node.js service to generate json out of data from Contentful
---
+++ src/contentful.ts
@@ -0,0 +1,39 @@
+import { createClient } from "contentful";
+import * as fs from "fs";
+require("dotenv").config();
+
+if (!(process.env.CONTENTFUL_SPACE && process.env.CONTENTFUL_TOKEN)) {
+    throw new Error("Contentful configuration isn't full");
+}
+
+const client = createClient({
+    space: process.env.CONTENTFUL_SPACE,
+    accessToken: process.env.CONTENTFUL_TOKEN
+});
+
+const writeToFile = (data: unknown, fileName: string) => {
+    fs.writeFileSync(`${fileName}.json`, JSON.stringify(data, null, 2));
+};
+
+const fetchEntriesAndWriteToFile = async ({
+    contentType,
+    fileName,
+    withAllLocales
+}: {
+    contentType: string;
+    fileName: string;
+    withAllLocales?: boolean;
+}) => {
+    try {
+        const entries = withAllLocales
+            ? await client.withAllLocales.withoutUnresolvableLinks.getEntries({ content_type: contentType, include: 10 })
+            : await client.getEntries({ content_type: contentType });
+
+        writeToFile(entries.items, fileName);
+        console.log(`Entries successfully written to ${fileName}.json`);
+    } catch (error) {
+        console.error(`Error fetching entries. Content type: ${contentType}`, error);
+    }
+};
+
+fetchEntriesAndWriteToFile({ contentType: "vault", fileName: "vaults" });
